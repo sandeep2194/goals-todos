85cd2c8954283266a445c2d081ee4b747ff38bbe
sandeep2194 <sandeep2194@live.com>
2021-05-21 16:42:35 +0530

    moved components from old project(reactnd-redux-todos-goals)

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,29 @@
+import React from 'react'
+import ConnectedTodos from './Todos'
+import ConnectedGoals from './Goals'
+import { connect } from 'react-redux'
+import {
+  handleInitialData,
+} from '../actions/shared'
 
-function App() {
-  return (
-    <div>
-      hello world
-    </div>
-  );
+class App extends React.Component {
+  async componentDidMount() {
+    const { dispatch } = this.props
+    dispatch(handleInitialData())
+  }
+  render() {
+    if (this.props.loading === true) {
+      return <h3>Loading</h3>
+    }
+    return (
+      <div>
+        <ConnectedTodos />
+        <ConnectedGoals />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default connect((state) => ({
+  loading: state.loading,
+}))(App)
